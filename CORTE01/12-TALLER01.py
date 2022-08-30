70398005de5aced84939707f6c3d0c47940c3e87
AlbertM11 <110635602+AlbertM11@users.noreply.github.com>
2022-08-29 22:03:26 -0500

    Update 12-TALLER01.py

--- CORTE01/12-TALLER01.py
+++ CORTE01/12-TALLER01.py
@@ -45,6 +45,59 @@ def f_stat (l_valores):
 
 ## 5. BONO (0.5)
 #TODO: realizar la verificación del punto aterior haciendo uso de la función assert (pytest)
+Punto N1:
+l_paises = ['Colombia','Mexico','Turquia','Polonia','serbia','dinamarca','holanda','Alemania']#Creamos la lista con los paises.
+l_paises1=""#Creamos una lista donde se añadran los paises en orden y con su respectiva mayuscula.
+for i in range(0,8,1):#Iniciamos un bucle para que recorra la lista.
+    l_paises1+=l_paises[i].capitalize()#Creamos funcion para las mayusculas en cada elemento de la lista.
+    l_paises1+=", "#Creamos funcion para que haya una coma y un espacio.
+archi1=open("datos.txt","w")#Definimos el archivo txt y le asignamos un nombre.
+with open('datos.txt', 'w') as temp_file:#Generamos el archivo txt y empezamos a escribir en el.
+    for item in l_paises1:#Iniciamos ciclo para la lista.
+        temp_file.write("%s" % item)#Escrbimos lo guardado en la lista en el txt.
+file = open('datos.txt', 'r')#Abrimos el erchivo txt.
+print(file.read())#Imprimimos la funcion para comprobar.
+
+Punto N2:
+str_archivo = open("datos.txt")#Abrimos el acrhivo txt.
+print(str_archivo.read())#Imprimimos en la consola el archivo.
+
+Punto N3:
+def f_calBin (s_num): #Iniciamos la funcion con el numero que se va a calcular (parametro).
+    s_num=s_num+0.0 #
+    print(s_num) #Imprimimos el numero.
+    s_residuo=0 #Iniciamos la variable residuo.
+    lista=[]
+    s_bin = []#Creamos dos listas vacias que llevaran las partes enteras del numero y otra el residuo.
+    str_num=str(s_num)
+    split_num=str_num.split(sep=".")
+    s1_num=int(split_num[0])
+    s2_num=int(split_num[1])#Iniciamos la  variables.
+
+    while s1_num!=0: #Iniciamos ciclo para s1.
+        s_residuo =s1_num % 2#Decimos que para residuo sera el resultado de la operacion.
+        s1_num=s1_num//2
+        lista.append(s_residuo)#Añadimos a la lista el residuo.
+    k = len(lista) - 1
+    while (k >= 0):#Iniciamos ciclo en el caso de k.
+        s_bin.append(lista[k])#Añadimos a la lista s_bin k.
+        k = k - 1
+    s_bin.append(".")#Añadimos un punto a la lista.
+
+    for i in range(4): #Iniciamos ciclo que recorra la lista en el rango 4.
+        notacion=len(str(s2_num))
+        s2_num=s2_num*(10**(-notacion))
+        s2_num=s2_num*2
+        s2_num=str(s2_num)
+        splits2_num=s2_num.split(sep=".")
+        s_bin.append(int(splits2_num[0]))
+        s2_num=int(splits2_num[1])
+        if s2_num==0:
+            break
+    s_bin="".join([str(_) for _ in s_bin])
+    return (f'El numero en decimal es: {s_bin}')#Retornamos la lista con el deecimal.
+print(f_calBin(45))#Imprimimos.
+Punto 4:
 import numpy as np #Importamos la libreria numpy.
 l_valores = [9,2, 9, 4, 5, 1, 8, 7, 8, 8, 9, 10,3]#Creamos una lista con los valores.
 def f_stat(l_valores):#Creamos una funcion para la media en el range de la lista.
