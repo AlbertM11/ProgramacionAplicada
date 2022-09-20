15442d48af47f92fb52469d83a6a8e31d966ec6f
AlbertM11 <110635602+AlbertM11@users.noreply.github.com>
2022-09-20 15:22:10 -0500

    Update 16-EJERCICO03.py

--- CORTE02/16-EJERCICO03.py
+++ CORTE02/16-EJERCICO03.py
@@ -41,4 +41,125 @@ print(f_contarconejos(13))#Para un año el resultado sera:
 #       Encontrar un vehiculo a partir de su dueño y determinar si está en el taller
 # TODO: 3 Todas las funciones que implemente deben estar en un script distinto al principal y donde se definen sus clases
 #       NOTA - (la UNICA exepción son las funciones que definen dentro de sus clases.)
+class Vehiculo():
+
+    fechaEntrega = "" 
+    costo = 0.0 
+    modelo = "" 
+    anio = 0 
+    duenio = None 
+
+    def _init_(self, fechaEntrega, costo, modelo, anio, duenio):
+        self.fechaEntrega = fechaEntrega 
+        self.costo = costo 
+        self.modelo = modelo 
+        self.anio = anio 
+        self.duenio = duenio 
+        
+    def informacion(self, fechaEntrega, costo, modelo, anio, duenio):
+     '''
+     A continuacion se utiliza el print para imprimir lo que se muestra en la linea siguiente en este caso cada uno de los parametros
+     en los que se usa el self para identificar que se establando de los parametros de la misma clase vehiculo 
+     '''
+        print
+        self.fechaEntrega
+        print
+        self.costo
+        print
+        self.modelo
+        print
+        self.anio
+        print
+        self.duenio
+mustang = vehiculo()
+mustang.fechaEntrega = '13/07/2023'
+mustang.costo= '200000000'
+mustang.modelo = 'Match 1'
+mustang.anio = '1965'
+mustang.duenio = 'Albert'
+mustang.print_information(mustang.d_delivery,mustang.cost,mustang.model,mustang.year,mustang.owner) 
+
+class Caja(): 
+    dineroTotal = 0.0 
+    movimientos = [] 
+    def _init_(self, dineroTotal): 
+        self.dineroTotal = dineroTotal 
+
+    def pagarEmpleados(self, empleados): 
+        totalAPagar = 0.0
+        i = 0
+        while i < len(empleados):
+            totalAPagar += empleados[i].salario 
+            i+=1 
+        if(self.dineroTotal >= totalAPagar):  
+            self.dineroTotal -= totalAPagar 
+            return True
+        return False
+
+    def ingresarDinero(self, dineroIngresado):
+        self.dineroTotal += dineroIngresado 
+  class GestionVehiculos():
+    vehiculos = [] 
+
+    def _init_(self, vehiculos):
+        self.vehiculos = vehiculos
+
+    def ingresarVehiculo(self, vehiculo, cantidadEmpleados):
+        if(len(self.vehiculos)<cantidadEmpleados): 
+            self.vehiculos.append(vehiculo)
+            return True
+        return False
+
+    def sacarVehiculo(self, vehiculo):
+        if(len(self.vehiculos)>0):
+            if(vehiculo in self.vehiculos):
+                self.vehiculos.remove(vehiculo)
+                return True
+        return False
+ class GestionEmpleado():
+    empleados = []
+
+    def _init_(self, empleados):
+        self.empleados = empleados
+
+    def buscarEmpleado(self, documento):
+        print("falta")
+
+    def contratarEmpleado(self, empleado):
+        self.empleados.append(empleado)
+        return True
+
+
+    def despedirEmpleado(self, empleado):
+        if(len(self.empleados) > 0):
+            self.empleados.remove(empleado)
+            return True
+        return False
+       
+from Vehiculo import Vehiculo
+
+
+class Empleado():
+    nombre = ""
+    cargo = ""
+    salario = 0.0
+    vehiculo = None
+    documento = 0
+
+    def _init_(self, nombre, cargo, salario, vehiculo, documento):
+        self.nombre = nombre
+        self.cargo = cargo
+        self.salario = salario
+        self.vehiculo = vehiculo
+        self.documento = documento
+        
+from Vehiculo import Vehiculo
+
+class Cliente():
+    nombre = ""
+    documento = 0
+
+    def _init_(self, nombre, documento):
+        self.nombre = nombre
+        self.documento = documento
 
